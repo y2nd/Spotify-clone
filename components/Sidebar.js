36f6da25842d21a183ade0583dc667938fa9e819
y2nd <y2nd@github.com>
2022-05-07 01:44:27 +0200

    login and logout are functional - wrote a NextJs middleware

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,64 +1,75 @@
-import React from 'react';
+import React from 'react'
+import { signOut, useSession } from 'next-auth/react'
+
 import {
-    HomeIcon,
-    SearchIcon,
-    LibraryIcon,
-    PlusCircleIcon,
-    HeartIcon,
-    RssIcon
-} from '@heroicons/react/outline';
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  HeartIcon,
+  RssIcon,
+} from '@heroicons/react/outline'
 
 function Sidebar() {
+  const { data: session, status } = useSession();
+
+  console.log(session);
+  
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-500">
-        <div className="space-y-4">
-            <button className="flex items-center space-x-2 hover:text-white">
-                <HomeIcon className="h-5 w-5"></HomeIcon>
-                <p>Home</p>
-            </button>    
-            <button className="flex items-center space-x-2 hover:text-white">
-                <SearchIcon className="h-5 w-5"></SearchIcon>
-                <p>Search</p>
-            </button> 
-            <button className="flex items-center space-x-2 hover:text-white">
-                <LibraryIcon className="h-5 w-5"></LibraryIcon>
-                <p>Your library</p>
-            </button> 
+    <div className="border-r border-gray-500 p-5 text-sm text-gray-500">
+      <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Logout</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="h-5 w-5"></HomeIcon>
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="h-5 w-5"></SearchIcon>
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="h-5 w-5"></LibraryIcon>
+          <p>Your library</p>
+        </button>
 
-            <hr className="border-t-[0.1px] border-gray-900"></hr>
+        <hr className="border-t-[0.1px] border-gray-900"></hr>
 
-            <button className="flex items-center space-x-2 hover:text-white">
-                <PlusCircleIcon className="h-5 w-5"></PlusCircleIcon>
-                <p>Create Playlist</p>
-            </button>    
-            <button className="flex items-center space-x-2 hover:text-white">
-                <HeartIcon className="h-5 w-5"></HeartIcon>
-                <p>Liked Songs</p>
-            </button> 
-            <button className="flex items-center space-x-2 hover:text-white">
-                <RssIcon className="h-5 w-5"></RssIcon>
-                <p>Your episodes</p>
-            </button> 
+        <button className="flex items-center space-x-2 hover:text-white">
+          <PlusCircleIcon className="h-5 w-5"></PlusCircleIcon>
+          <p>Create Playlist</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HeartIcon className="h-5 w-5"></HeartIcon>
+          <p>Liked Songs</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <RssIcon className="h-5 w-5"></RssIcon>
+          <p>Your episodes</p>
+        </button>
 
-            <hr className="border-t-[0.1px] border-gray-900"></hr>
+        <hr className="border-t-[0.1px] border-gray-900"></hr>
 
-            {/* Playlist */}
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-            <p className="cursor-pointer hover:text-white">Playlist name...</p>
-
-        </div>
+        {/* Playlist */}
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+      </div>
     </div>
   )
 }
--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import {getProviders, signIn } from "next-auth/react";
-
+import React from 'react'
+import { getProviders, signIn } from 'next-auth/react'
 
 function Login({ providers }) {
   return (
-    <div>
-      <h1> This is a login page </h1>
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+    	<img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="Spotiofy logo image"></img>
+
+		{/* instead of hard coding a buttobn, iterate through all providers and make a button for each one */}
+		{Object.values(providers).map( (provider) => (
+			<div key={provider.name}>
+				<button className="bg-[#18D860] text-white p-5 rounded-full" onClick={() => signIn(provider.id, {callbackUrl: '/'})}>Login with {provider.name}</button>
+			</div>
+		))}
+
     </div>
   )
 }
@@ -16,8 +23,8 @@ export async function getServerSideProps() {
   const providers = await getProviders();
 
   return {
-	  props: {
-		providers
-	  }
+    props: {
+      providers,
+    },
   }
 }
