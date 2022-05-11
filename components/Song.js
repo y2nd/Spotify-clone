3c8251ac0068173b9763b8b949cce910082df91f
y2nd <y2nd@github.com>
2022-05-11 11:43:36 +0200

    able to play a song on a working device

--- components/Song.js
+++ components/Song.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useRecoilState } from 'recoil';
 import { millisToMinutesAndSeconds as duration} from '../lib/time';
+import { currentTrackIdState, isPlayingState } from '../atoms/songAtom';
+import useSpotify from '../hooks/useSpotify'; 
 
 function Song({ order, track }) {
+  const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+  const playSong = () => {
+    setCurrentTrackId(track.track.id);
+    setIsPlaying(true);
+
+    spotifyApi.play({
+      uris: [track.track.uri],
+    });
+  }
+
   return (
-    <div className="grid grid-cols-2  text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
+    <div className="grid grid-cols-2  text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer" onClick={playSong}>
         <div className="flex items-center space-x-4">
             <p>{order + 1}</p>
             <img src={track.track.album.images[0]?.url} alt="" className="h-10 w-10"></img>
