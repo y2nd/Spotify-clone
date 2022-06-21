5b86887cd7115732e1122197fa03fac964344c5f
Youssef T <sheese.711@gmail.com>
2022-06-21 16:32:16 +0200

    typos

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Spotify-clone
 A web app Spotify clone I built as part of my training to learn Nextjs. Not finished due to some problems of incompatibility between Recoil and NextJs.
-Overall the app works just fine, it plays music and it successfully authenticate the user using their Spotify crendentials.
+Overall the app works just fine, it plays music and it successfully authenticates the user using their Spotify crendentials.
--- pages/login.js
+++ pages/login.js
@@ -6,7 +6,7 @@ function Login({ providers }) {
     <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
     	<img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="Spotiofy logo image"></img>
 
-		{/* instead of hard coding a buttobn, iterate through all providers and make a button for each one */}
+		{/* instead of hard coding a button, iterate through all providers and make a button for each one */}
 		{Object.values(providers).map( (provider) => (
 			<div key={provider.name}>
 				<button className="bg-[#18D860] text-white p-5 rounded-full" onClick={() => signIn(provider.id, {callbackUrl: '/'})}>Login with {provider.name}</button>
