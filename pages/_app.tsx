b14f44eb3f63e4db0391eeb1bef5e78827aeacc6
Youssef T <sheese.711@gmail.com>
2022-06-21 15:33:59 +0200

    fixed some errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,21 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from 'recoil';
+import Head from "next/head";
+
+
 
 function MyApp({ Component, pageProps: { session, ...pageProps} }: AppProps) {
   return (
+    
     <SessionProvider session={session}>
       <RecoilRoot>  
+      <Head>
+        <title>Spotify Clone</title>
+      </Head>
         <Component {...pageProps} />
       </RecoilRoot>
     </SessionProvider>
-    
   )
 }
 
